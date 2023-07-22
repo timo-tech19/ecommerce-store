8b67c236a3b879359a0d3f1f469507949bf9766c
Timo Heman <timoheman16@gmail.com>
2023-07-22 20:56:37 +0100

    setup ecommerce store

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+	return (
+		<div className='bg-white border-t'>
+			<div className='mx-auto py-10'>
+				<p className='text-center text-xs text-black'>
+					Timo Heman &copy; 2023 Ecommerce Store. All rights reserved
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
