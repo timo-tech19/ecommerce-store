9ce475cffca5ec66d4d47908651b8c71b973c96d
Timo Heman <timoheman16@gmail.com>
2023-07-25 12:10:52 +0100

    added billboard to store ui

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-	return <div>Homepage</div>;
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+export default async function Home() {
+	const billboard = await getBillboard("cbce06d1-5646-4467-8544-5fa5c2a53dea");
+
+	return (
+		<Container>
+			<div className='space-y-10 pb-10'>
+				<Billboard data={billboard} />
+			</div>
+		</Container>
+	);
 }
