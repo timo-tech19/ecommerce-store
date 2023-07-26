2662ebea92928871c92dcdcf1b0047660a3b91a6
Timo Heman <timoheman16@gmail.com>
2023-07-26 13:29:54 +0100

    added checkout page

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -9,12 +9,14 @@ import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCardProps {
 	data: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+	const cart = useCart();
 	const previewModal = usePreviewModal();
 	const router = useRouter();
 
@@ -24,6 +26,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
 		event.stopPropagation();
 
 		previewModal.onOpen(data);
+	};
+
+	const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+		event.stopPropagation();
+
+		cart.addItem(data);
 	};
 
 	return (
@@ -46,6 +54,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
 							icon={<Expand size={20} className='text-gray-600' />}
 						/>
 						<IconButton
+							onClick={onAddToCart}
 							icon={<ShoppingCart size={20} className='text-gray-600' />}
 						/>
 					</div>
